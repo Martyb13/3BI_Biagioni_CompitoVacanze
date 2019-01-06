7eac00e05dcf3789103e833525bd50e0153ce1b8
martina biagioni <martinabiagioni12@gmail.com>
2019-01-06 16:53:52 +0100

    Si inserisce un controllo dalla riga 17 che verifichi se il numero fosse minore del numero massimo e si inserisce un commento alla riga 21, dove spiega il valore di due variabili e la loro conversione

--- Compito/Program.cs
+++ Compito/Program.cs
@@ -14,8 +14,12 @@ namespace Compito
             //a corrisponde al valore massimo che può essere inserito
             Console.WriteLine(a);
             //si somma 1 ad "a" e si ottiene il valore minimo
-            Console.WriteLine(a + 1);
-            int b = 128;
+            if (a < int.MaxValue)
+            {
+                Console.WriteLine(a + 1);
+            }
+            // si dà a "b" il valore di 128 e a "c" la sua negaione, poi si trasformano in binario
+            int b = -128;
             int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
             Console.WriteLine(Convert.ToString(c, 2));
