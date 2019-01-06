754321dccf520ec8728d87ae87c6c57218be0c79
martina biagioni <martinabiagioni12@gmail.com>
2019-01-06 17:07:07 +0100

    Si richiede all'utente l'inserimento di un numero e viene stampata la sua negazione e lam somma tra il numero e la sua negazione

--- Compito/Program.cs
+++ Compito/Program.cs
@@ -23,12 +23,13 @@ namespace Compito
             int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
             Console.WriteLine(Convert.ToString(c, 2));
-            int value = 31;
+            Console.WriteLine("inserisci un numero");
+            int value = Convert.ToInt32(Console.ReadLine());
             int somma = value;
             value = ~value + 1;  //NOT
             //value = value + 1;
             somma = somma + value;
-            Console.WriteLine($"{value} e {somma}");
+            Console.WriteLine($"la negazione del valore è {value} e la somma tra valor einserito e negazione è {somma}");
             long number = 55555;
             Console.WriteLine(Convert.ToString(number, 2));
             short number1 = (short)number;
